fd80a51f7e0829d4f75256cf7a05a665cae15adf
sd vm <95736679+vipv007@users.noreply.github.com>
2024-12-25 13:01:51 +0530

    Update api.service.ts

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -7,7 +7,8 @@ import { catchError, map } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class ApiService {
-  private apiUrl = 'https://celescontainerwebapp-server.mongo.cosmos.azure.com:443/items';
+  // private apiUrl = 'https://celescontainerwebapp-server.mongo.cosmos.azure.com:443/items';
+  private apiUrl = 'https://celescontainerwebapp-server-westus3.mongo.cosmos.azure.com/items';
   //private apiUrl = 'http://localhost:5000/items';
 
   constructor(private http: HttpClient) {}
